109e4ddd0d3301b1e78a20e786b0b66ae96c8aea
Mateja Petrovic <mateja176@gmail.com>
2019-04-11 02:00:08 +0200

    Runs build

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 exports.__esModule = true;
-var actions = require("./actions");
-exports.actions = actions;
+__export(require("./actions"));
